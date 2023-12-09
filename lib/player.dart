be730555201c7d383a05a962dd46e8bf7ad78d0c
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2023-12-09 11:47:02 +0100

    making anchor topleft

--- lib/player.dart
+++ lib/player.dart
@@ -2,17 +2,21 @@ import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
 class Player extends PositionComponent {
+  Player({this.playerRadius = 15});
+
   final _velocity = Vector2.zero();
   final _gravity = 980.0;
   final _jumpSpeed = 350.0;
   bool _jumpingRight = true;
 
+  final double playerRadius;
+
   @override
   void onMount() {
     position = Vector2(0, 0);
-    size = Vector2(100, 100);
+    size = Vector2.all(playerRadius * 2);
+    anchor = Anchor.topLeft;
     super.onMount();
-    
   }
 
   @override
@@ -25,7 +29,11 @@ class Player extends PositionComponent {
   @override
   void render(Canvas canvas) {
     super.render(canvas);
-    canvas.drawCircle(position.toOffset(), 15, Paint()..color = Colors.yellow);
+    canvas.drawCircle(
+      const Offset(0, 0) + (size / 2).toOffset(),
+      playerRadius,
+      Paint()..color = Colors.yellow,
+    );
   }
 
   void jump() {
