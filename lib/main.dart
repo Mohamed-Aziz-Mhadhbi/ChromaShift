54487a18fcb459a8a943a3fd846f4b2faacacd03
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2023-12-09 09:30:30 +0100

    Player done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flame/components.dart';
+import 'package:chroma_shift/player.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
@@ -25,34 +25,5 @@ class MyGame extends FlameGame with TapCallbacks {
   void onTapDown(TapDownEvent event) {
     myPlayer.jump();
     super.onTapDown(event);
-  }
-}
-
-class Player extends PositionComponent {
-  final _velocity = Vector2(0, 30.0);
-  final _gravity = 980.0;
-  final _jumpSpeed = 350.0;
-
-  @override
-  void onMount() {
-    super.onMount();
-    position = Vector2(100, 100);
-  }
-
-  @override
-  void update(double dt) {
-    super.update(dt);
-    position += _velocity * dt;
-    _velocity.y += _gravity * dt;
-  }
-
-  @override
-  void render(Canvas canvas) {
-    super.render(canvas);
-    canvas.drawCircle(position.toOffset(), 15, Paint()..color = Colors.yellow);
-  }
-
-  void jump() {
-    _velocity.y = -_jumpSpeed;
   }
 }
