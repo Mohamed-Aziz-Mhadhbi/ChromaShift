0aae4837e383d6f77a405b0176f44c8273fff743
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2023-12-10 07:15:16 +0100

    using the key as static const the correct why

--- lib/ground.dart
+++ lib/ground.dart
@@ -4,11 +4,12 @@ import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
 class Ground extends PositionComponent {
+  static const String keyname = 'single_ground_key';
   Ground({required super.position})
       : super(
           size: Vector2(100, 1),
           anchor: Anchor.center,
-          key: ComponentKey.named("ground"),
+          key: ComponentKey.named(keyname),
         );
   @override
   void render(Canvas canvas) {
