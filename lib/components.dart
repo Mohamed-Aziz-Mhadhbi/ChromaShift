c799e7f4ca0e470519ac427cf4a672aef9845f6f
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2023-12-10 15:07:33 +0100

    Making the colors and the size of the obstacles (components) random

--- lib/components.dart
+++ lib/components.dart
@@ -1,21 +1,34 @@
+import 'dart:math';
+
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
 class ComponentsGame extends PositionComponent {
   ComponentsGame({
-    required super.position,
-    required super.size,
+    required Vector2 position,
+    required Vector2 size,
     this.thickness = 8,
-  })  : assert(size!.y == size.x),
-        super(anchor: Anchor.center);
+  })  : assert(size.y == size.x),
+        super(
+          position: position,
+          size: size,
+          anchor: Anchor.center,
+        );
 
   final double thickness;
+
+  final randomColor = Color.fromARGB(
+    255,
+    Random().nextInt(256),
+    Random().nextInt(256),
+    Random().nextInt(256),
+  );
 
   @override
   void render(Canvas canvas) {
     canvas.drawRect(
-      Rect.fromLTWH(0, 0, size.x / 2, height),
-      Paint()..color = Colors.blue,
+      Rect.fromLTWH(0, 0,  Random().nextDouble() *  (size.x / 2), Random().nextDouble() *  (size.y / 2)),
+      Paint()..color = randomColor,
     );
     super.render(canvas);
   }
