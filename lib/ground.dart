bfa9311eb01321f3abdd8ef149bc55e68d79ac21
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2023-12-10 06:58:46 +0100

    adding rectangle as gound

--- lib/ground.dart
+++ lib/ground.dart
@@ -1,5 +1,19 @@
+import 'dart:ui';
+
 import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
 
 class Ground extends PositionComponent {
-  Ground({required super.position}): super(size: Vector2(100, 1));
+  Ground({required super.position})
+      : super(
+          size: Vector2(100, 1),
+        );
+  @override
+  void render(Canvas canvas) {
+    canvas.drawRect(
+      Rect.fromLTWH(0, 0, width, height),
+      Paint()..color = Colors.red,
+    );
+    super.render(canvas);
+  }
 }
